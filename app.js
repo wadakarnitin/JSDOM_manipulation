b8d530fb271682e6fa0fe55cd2d17b623263e1fa
Nitin Wadakar <wadakarnitin@gmail.com>
2020-03-23 09:46:07 +0530

    node concept add in practice.js

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-let wmf=document.querySelector('#book-list ul li:nth-child(2) .name');
-
-console.log(wmf);
-
-let allelements=document.querySelectorAll("#book-list li .name");
-
-Array.from(allelements).forEach(item=>{
-    console.log(item);
-})
 
--- practice.js
+++ practice.js
@@ -6,3 +6,27 @@ console.log(Array.isArray(Array.from(titles))); //converts collection to array
 Array.from(titles).forEach(item=>{
     console.log(item);
 })
+
+//getElementByClassName gives output in html collection , further to traverse it we convert into array first n then foreach
+//querySelectorAll gives nodelist which is array like on which we can directly use forEach
+
+let wmf=document.querySelector('#book-list ul li:nth-child(2) .name');
+
+console.log(wmf);
+
+let allelements=document.querySelectorAll("#book-list li .name");
+
+allelements.forEach(item=>{
+    console.log(item);
+})
+
+let banner = document.querySelector('#page-banner');
+console.log(banner.nodeType);//1
+console.log(banner.nodeName);//DIV
+console.log(banner.hasChildNodes())//true
+
+let clonedNode=banner.cloneNode(true);//writing true here clones the whole node childs and all 
+console.log(clonedNode);
+
+//Node : every element on dom is a node 
+
